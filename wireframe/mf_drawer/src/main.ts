46807476958d84777fcd8459cdb6bfd8636be22f
Scalco7 <fscalco7@gmail.com>
2024-06-27 17:23:35 -0300

    navigation

--- wireframe/mf_drawer/src/main.ts
+++ wireframe/mf_drawer/src/main.ts
@@ -1,2 +1,15 @@
 import './styles/reset.css'
 import './styles/style.css'
+
+import { navigateTo, getRoute } from './scripts/navigation'
+
+const videoButton = document.getElementById('video-button')
+const favoriteButton = document.getElementById('favorite-button')
+
+videoButton!.addEventListener('click', () => {
+  navigateTo('videos')
+})
+
+favoriteButton!.addEventListener('click', () => {
+  navigateTo('favoritos')
+})
