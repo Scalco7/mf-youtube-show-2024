308111cf04ec974a3a7bc8c8542fb5e2b9288787
Scalco7 <fscalco7@gmail.com>
2024-06-28 09:28:56 -0300

    favorite_service

---
+++ api/src/services/favorite.service.ts
@@ -0,0 +1,34 @@
+import { Favorite } from "../migrations/entities/favorite.entity";
+
+export class FavoriteService {
+    async addFavoriteVideo(userId: string, videoId: string): Promise<Favorite> {
+        return Favorite.create({ user_id: userId, video_id: videoId })
+    }
+
+    async removeFavoriteVideo(userId: string, videoId: string): Promise<void> {
+        let favorite
+        try {
+            favorite = await Favorite.findOne({ where: { user_id: userId, video_id: videoId } })
+        } catch (err) {
+            throw 'Erro buscando video favorito'
+        }
+
+        if (!favorite)
+            throw 'Video favorito não existe'
+
+        return favorite.destroy()
+    }
+
+    async listFavoriteByUserId(userId: string): Promise<string[]> {
+        let favoriteList: string[] = [];
+
+        try {
+            const favoriteQuery = await Favorite.findAll({ where: { user_id: userId }, attributes: ['video_id'] })
+            favoriteList = favoriteQuery.map((fav) => fav.dataValues.video_id)
+        } catch (err) {
+            throw 'erro ao buscar no banco de dados'
+        }
+
+        return favoriteList
+    }
+} 
