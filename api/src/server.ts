e24d3bce50240ac286132a81736c7aa14378c8b5
Scalco7 <fscalco7@gmail.com>
2024-06-28 11:27:17 -0300

    temp-server

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,9 +3,15 @@ import { Router, Request, Response } from 'express'
 
 import { User } from './migrations/entities/user.entity';
 import { migrate } from './migrations/migrate';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { FavoriteService } from './services/favorite.service';
 
 const app = express()
 const route = Router()
+const userService = new UserService()
+const authService = new AuthService()
+const favoriteService = new FavoriteService()
 
 migrate()
 
@@ -17,13 +23,52 @@ route.get('/', (req: Request, res: Response) => {
 
 route.get('/users', async (req: Request, res: Response) => {
     const users = await User.findAll()
+    const userId = users[0].dataValues.id
+
+    try {
+        console.log(await userService.getUserNameById(userId))
+        console.log(await authService.login('rafael12345@gmail.com', '123456'))
+
+        console.log(await favoriteService.listFavoriteByUserId(userId))
+    }
+    catch (err) {
+        console.log("erro - ", err)
+    }
+
     res.json({ users: users })
 })
 
+route.post('/add-favorite', async (req: Request, res: Response) => {
+    const users = await User.findAll()
+    const userId = users[0].dataValues.id
+
+    try {
+        await favoriteService.addFavoriteVideo(userId, req.body.video_id)
+    } catch (err) {
+        console.log("erro - ", err)
+    }
+
+    res.json({ favorites: await favoriteService.listFavoriteByUserId(userId) })
+})
+
+route.delete('/remove-favorite', async (req: Request, res: Response) => {
+    const users = await User.findAll()
+    const userId = users[0].dataValues.id
+
+    try {
+        await favoriteService.removeFavoriteVideo(userId, req.body.video_id)
+    } catch (err) {
+        console.log("erro - ", err)
+    }
+
+    res.json({ favorites: await favoriteService.listFavoriteByUserId(userId) })
+})
+
 route.post('/user', async (req: Request, res: Response) => {
-    const user = await User.create(req.body);
+    const user = await userService.create(req.body.name, req.body.email, req.body.password)
     res.json(user);
 })
+
 
 app.use(route)
 
