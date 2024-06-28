58aaf959b2c5dc00b3edc98af5bc7518a6d472a8
Scalco7 <fscalco7@gmail.com>
2024-06-28 05:50:30 -0300

    test-routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,28 @@
 import express from 'express'
 import { Router, Request, Response } from 'express'
 
+import { User } from './migrations/entities/user.entity';
+import { migrate } from './migrations/migrate';
+
 const app = express()
 const route = Router()
+
+migrate()
 
 app.use(express.json())
 
 route.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Hello World' })
+})
+
+route.get('/users', async (req: Request, res: Response) => {
+    const users = await User.findAll()
+    res.json({ users: users })
+})
+
+route.post('/user', async (req: Request, res: Response) => {
+    const user = await User.create(req.body);
+    res.json(user);
 })
 
 app.use(route)
