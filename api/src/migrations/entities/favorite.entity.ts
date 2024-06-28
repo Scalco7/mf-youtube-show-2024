15a015ecf34658d7272e10ce7bf4bf5377dd1dcc
Scalco7 <fscalco7@gmail.com>
2024-06-28 05:37:11 -0300

    favorite-entity

---
+++ api/src/migrations/entities/favorite.entity.ts
@@ -0,0 +1,26 @@
+import { DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import { sequelize } from "../../database/database";
+
+class Favorite extends Model<InferAttributes<Favorite>, InferCreationAttributes<Favorite>> {
+    declare user_id: number;
+    declare video_id: string;
+    declare created_at: Date;
+}
+
+Favorite.init({
+    user_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+    },
+    video_id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        primaryKey: false,
+        allowNull: false,
+    }
+}, { sequelize, modelName: 'favorite' })
