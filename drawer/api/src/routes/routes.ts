99e97abc1c5f30da0bd2c79631b1c3a7fdada8a9
Scalco7 <fscalco7@gmail.com>
2024-06-29 20:28:52 -0300

    routes finish

--- drawer/api/src/routes/routes.ts
+++ drawer/api/src/routes/routes.ts
@@ -1,8 +1,16 @@
 import { Request, Response, Router } from "express";
+import { Controller } from "../controllers/controller";
+
+const controller = new Controller()
 
 export const route = Router()
 
-route.get('/count-favorites/:userId', (req: Request, res: Response) => {
-    console.log(req.params.userId)
-    res.json({ status: 200 })
+route.get('/count-favorites/:userId', async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId
+        const count = await controller.countFavoritesByUserId({ userId })
+        res.json({ status: 200, favoritesCount: count })
+    } catch (error) {
+        res.json({ status: 400, error: error })
+    }
 })
