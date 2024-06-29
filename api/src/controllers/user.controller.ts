f2dfe9819f76b67dc65b26756193be9e48fe7a50
Scalco7 <fscalco7@gmail.com>
2024-06-29 18:21:58 -0300

    user controller refactor

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -2,6 +2,18 @@ import { User } from "../migrations/entities/user.entity";
 import { UserService } from "../services/user.service";
 import Joi from "joi";
 
+const userDataObjectValidator = Joi.object({
+    name: Joi.string().required().min(5),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(6)
+})
+
+interface ICreateUserData {
+    name: string
+    email: string
+    password: string
+}
+
 export class UserController {
     private userService: UserService
 
@@ -9,13 +21,8 @@ export class UserController {
         this.userService = new UserService()
     }
 
-    async createUser(user: { name: string, email: string, password: string }): Promise<User> {
-        const userObject = Joi.object({
-            name: Joi.string().required().min(5),
-            email: Joi.string().email().required(),
-            password: Joi.string().required().min(6)
-        })
-        const { error } = userObject.validate(user)
+    public async createUser(user: ICreateUserData): Promise<User> {
+        const { error } = userDataObjectValidator.validate(user)
 
         if (error)
             throw ('dados inválidos - ' + error)
