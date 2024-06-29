ccbfead84542e98a2a3b57af6598221fbae9d1f8
Scalco7 <fscalco7@gmail.com>
2024-06-29 20:43:20 -0300

    finish controller

--- drawer/api/src/controllers/controller.ts
+++ drawer/api/src/controllers/controller.ts
@@ -1,10 +1,26 @@
+import Joi from "joi"
+import { Service } from "../services/service"
+
 interface ICountFavoritesByUserIdData {
     userId: string
 }
 
 export class Controller {
+    private service: Service
+
+    constructor() {
+        this.service = new Service()
+    }
+
     public async countFavoritesByUserId(data: ICountFavoritesByUserIdData): Promise<number> {
-        const count = 1
+        try {
+            Joi.assert(data.userId, Joi.string().required())
+        }
+        catch (error) {
+            throw (error)
+        }
+
+        const count = await this.service.countFavoritesByUserId(data.userId)
         return count
     }
 }
