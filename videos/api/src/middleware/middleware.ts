0b1f9563613cffdf100e4305ce6487ba21e953cf
Scalco7 <fscalco7@gmail.com>
2024-06-30 05:18:01 -0300

    middlaware

---
+++ videos/api/src/middleware/middleware.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+export function bearerAuthentication(req: Request, res: Response, next: NextFunction) {
+    let token = req.header('Authorization');
+    if (!token) return res.status(401).json({ error: 'Access denied' })
+    try {
+        token = token.replace('Bearer ', '')
+        const decoded = verify(token, process.env.JWT_SECRET_KEY!.toString())
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+}
