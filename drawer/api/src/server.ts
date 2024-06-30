5014dece9ff7262804ce7459bb587e74c175cb5f
Scalco7 <fscalco7@gmail.com>
2024-06-30 04:01:26 -0300

    cors

--- drawer/api/src/server.ts
+++ drawer/api/src/server.ts
@@ -2,11 +2,13 @@ import express from 'express'
 
 import dotenv from 'dotenv'
 import { route } from './routes/routes'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/', route)
 app.listen(process.env.PORT, () => console.log("heyy, api is listen on port: " + process.env.PORT))
