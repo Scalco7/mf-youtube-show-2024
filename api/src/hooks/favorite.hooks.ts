fe7d669882a690c34000dcf8ee26e972b541b197
Scalco7 <fscalco7@gmail.com>
2024-07-01 19:26:03 -0300

    hook finish

--- api/src/hooks/favorite.hooks.ts
+++ api/src/hooks/favorite.hooks.ts
@@ -1,10 +1,18 @@
-import { Favorite } from "../migrations/entities/favorite.entity";
+import axios from "axios";
 import { FavoriteService } from "../services/favorite.service";
 
 const favoriteService = new FavoriteService()
 
 export async function alertNewFavoriteLength(userId: string): Promise<void> {
-    const newLength = (await favoriteService.listFavoriteByUserId(userId)).length
-
-    // axios 
+    try {
+        const newLength = (await favoriteService.listFavoriteByUserId(userId)).length
+        const url = `${process.env.DRAWER_API_URL}/update-favorites-length`
+        const body = {
+            userId,
+            newLength
+        }
+        axios.post(url, body)
+    } catch (error) {
+        console.log(error)
+    }
 }
