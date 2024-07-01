a5e5eb4c1845332d392a741bcd566165d8cb5845
Scalco7 <fscalco7@gmail.com>
2024-07-01 11:33:26 -0300

    css / html updates

--- drawer/frontend/src/main.ts
+++ drawer/frontend/src/main.ts
@@ -23,3 +23,28 @@ document.addEventListener("DOMContentLoaded", () => {
     favoriteButton?.classList.add("current");
   }
 });
+
+
+// const socket = new WebSocket('ws://example.com/socket');
+
+// // Evento disparado quando a conexão é aberta
+// socket.onopen = () => {
+//     console.log('Conexão WebSocket aberta');
+//     // Enviando uma mensagem ao servidor
+//     socket.send('Olá, servidor!');
+// };
+
+// // Evento disparado quando uma mensagem é recebida do servidor
+// socket.onmessage = (event) => {
+//     console.log('Mensagem recebida: ', event.data);
+// };
+
+// // Evento disparado quando a conexão é fechada
+// socket.onclose = () => {
+//     console.log('Conexão WebSocket fechada');
+// };
+
+// // Evento disparado em caso de erro
+// socket.onerror = (error) => {
+//     console.error('Erro no WebSocket: ', error);
+// };
