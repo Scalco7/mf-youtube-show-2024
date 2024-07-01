971b77b2ed92fb41f4b8e8be406c5d932c32c1ca
Scalco7 <fscalco7@gmail.com>
2024-07-01 20:44:57 -0300

    auth protection

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { bearerAuthentication } from "../middleware/middleware";
 
 const authController = new AuthController();
 
 export const authRoute = Router();
+
+authRoute.use("/", bearerAuthentication);
 
 authRoute.post("/login", async (req: Request, res: Response) => {
   try {
