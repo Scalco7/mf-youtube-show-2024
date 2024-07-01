a257f592e45955f4682c6b12d6c22a7e9632e38d
Scalco7 <fscalco7@gmail.com>
2024-07-01 04:23:27 -0300

    update

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,23 +1,21 @@
-import express from "express";
-import { Router, Request, Response } from "express";
+import express from 'express'
+import { Router } from 'express'
 
-import { migrate } from "./migrations/migrate";
-import dotenv from "dotenv";
-import { userRoute } from "./routes/user.routes";
-import { favoriteRoute } from "./routes/favorite.routes";
-import { authRoute } from "./routes/auth.routes";
+import { migrate } from './migrations/migrate';
+import dotenv from 'dotenv'
+import { userRoute } from './routes/user.routes';
+import { favoriteRoute } from './routes/favorite.routes';
+import { authRoute } from './routes/auth.routes';
 
-dotenv.config();
-const app = express();
-const route = Router();
+dotenv.config()
+const app = express()
+const route = Router()
 
-migrate();
-app.use(express.json());
+migrate()
+app.use(express.json())
 
-app.use("/auth", authRoute);
-app.use("/users", userRoute);
-app.use("/favorites", favoriteRoute);
-app.use(route);
-app.listen(process.env.PORT, () =>
-  console.log("heyy, api is listen on port: " + process.env.PORT),
-);
+app.use('/auth', authRoute)
+app.use('/users', userRoute)
+app.use('/favorites', favoriteRoute)
+app.use(route)
+app.listen(process.env.PORT, () => console.log("heyy, api is listen on port: " + process.env.PORT))
