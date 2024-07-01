d1d1b53fe8b0ab99732be0bb5880803286decc99
Scalco7 <fscalco7@gmail.com>
2024-07-01 20:51:10 -0300

    regsiter route

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -16,3 +16,12 @@ authRoute.post("/login", async (req: Request, res: Response) => {
     res.status(400).json({ error: error });
   }
 });
+
+authRoute.post("/register", async (req: Request, res: Response) => {
+  try {
+    const token = await authController.registerUser(req.body);
+    res.status(200).json({ token: token });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
--- api/src/validators/auth.validators.ts
+++ api/src/validators/auth.validators.ts
@@ -1,6 +1,12 @@
 import Joi from "joi";
 
-export const ILogiDataValidator = Joi.object({
+export const ILoginDataValidator = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6),
+});
+
+export const IRegisterDataValidator = Joi.object({
+  name: Joi.string().required().min(5),
   email: Joi.string().email().required(),
   password: Joi.string().required().min(6),
 });
