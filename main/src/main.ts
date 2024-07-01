ded901cdcff07f1a529e910312d730e2abfa412e
Scalco7 <fscalco7@gmail.com>
2024-07-01 11:06:03 -0300

    integrate iframes basic

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,2 +1,19 @@
+import { getRoute } from './scripts/navigation';
 import './styles/reset.css'
 import './styles/style.css'
+
+const token = localStorage.getItem('token')
+
+const drawerIframe: HTMLIFrameElement = document.createElement('iframe');
+drawerIframe.src = `http://localhost:5175${getRoute()}?token=${token}`;
+drawerIframe.width = "250px";
+drawerIframe.height = "100%";
+
+const videoIframe: HTMLIFrameElement = document.createElement('iframe');
+videoIframe.src = `http://localhost:5174${getRoute()}?token=${token}`;
+// videoIframe.width = "250px";
+videoIframe.height = "100%";
+
+const containerElement: HTMLElement | null = document.getElementById('app');
+containerElement!.appendChild(drawerIframe);
+containerElement!.appendChild(videoIframe);
--- videos/frontend/src/scripts/utils/navigation.ts
+++ videos/frontend/src/scripts/utils/navigation.ts
@@ -1,3 +1,9 @@
 export function getRoute(): string {
   return window.location.pathname;
 }
+
+export function getParams(): URLSearchParams {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams;
+}
