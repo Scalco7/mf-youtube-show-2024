4ebc6800a576589482fe253e9acf0f2c40297a68
Scalco7 <fscalco7@gmail.com>
2024-07-02 12:56:00 -0300

    api update

--- drawer/api/src/routes/routes.ts
+++ drawer/api/src/routes/routes.ts
@@ -2,15 +2,16 @@ import { Request, Response, Router } from "express";
 import { Controller } from "../controllers/controller";
 import { bearerAuthentication } from "../middleware/middleware";
 import { io } from "../server";
+import { getUserIdByToken } from "../utils/getUserIdByToken";
 
 const controller = new Controller();
 export const route = Router();
 
-route.get("/count-favorites/:userId", bearerAuthentication, async (req: Request, res: Response) => {
+route.get("/count-favorites", bearerAuthentication, async (req: Request, res: Response) => {
   try {
-    const userId = req.params.userId;
+    const userId = getUserIdByToken(req.header("Authorization") ?? '')
     const count = await controller.countFavoritesByUserId({ userId });
-    res.status(200).json({ favoritesCount: count });
+    res.status(200).json({ favoritesQuantity: count });
   } catch (error) {
     res.status(400).json({ error: error });
   }
