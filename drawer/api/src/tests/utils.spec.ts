7258eb0c6641749719e133a7da9564f824a7138b
Scalco7 <fscalco7@gmail.com>
2024-07-03 19:16:28 -0300

    utils test drawer api

---
+++ drawer/api/src/tests/utils.spec.ts
@@ -0,0 +1,15 @@
+import { getUserIdByToken } from "../utils/getUserIdByToken";
+
+const oldToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM2ZGEyM2ZlLTliOTktNDg4Yy05MmQyLTg1OWI3OWFiNzUxMyIsIm5hbWUiOiJmZWxpcGUiLCJpYXQiOjE3MTk5NjA5NTUsImV4cCI6MTcxOTk4MjU1NX0.--r4pkfsAXAq-_q4YrGVbGYKAUajJoV7PTzgoc9zRiA'
+
+describe('Utils', () => {
+    describe('GetUserById', () => {
+        test('Should throw Token invalido for token: ""', () => {
+            expect(() => { getUserIdByToken('') }).toThrow('Token invalido')
+        })
+
+        test('Should throw Token expirado for expirated token', () => {
+            expect(() => { getUserIdByToken(oldToken) }).toThrow('Token expirado')
+        })
+    })
+})
