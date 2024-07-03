dd4f5258e59f2d1d0fe5779b47e49bd168b24d92
Scalco7 <fscalco7@gmail.com>
2024-07-03 18:57:21 -0300

    update utils test

--- videos/api/src/tests/utils.spec.ts
+++ videos/api/src/tests/utils.spec.ts
@@ -2,12 +2,14 @@ import { getUserIdByToken } from "../utils/getUserIdByToken";
 
 const oldToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM2ZGEyM2ZlLTliOTktNDg4Yy05MmQyLTg1OWI3OWFiNzUxMyIsIm5hbWUiOiJmZWxpcGUiLCJpYXQiOjE3MTk5NjA5NTUsImV4cCI6MTcxOTk4MjU1NX0.--r4pkfsAXAq-_q4YrGVbGYKAUajJoV7PTzgoc9zRiA'
 
-describe('Testing getUserById function', () => {
-    test('Test getUserById without Token', () => {
-        expect(() => { getUserIdByToken('') }).toThrow('Token invalido')
-    })
+describe('Utils', () => {
+    describe('GetUserById', () => {
+        test('Should throw Token invalido for token: ""', () => {
+            expect(() => { getUserIdByToken('') }).toThrow('Token invalido')
+        })
 
-    test('Test getUserById with an old Token', () => {
-        expect(() => { getUserIdByToken(oldToken) }).toThrow('Token expirado')
+        test('Should throw Token expirado for expirated token', () => {
+            expect(() => { getUserIdByToken(oldToken) }).toThrow('Token expirado')
+        })
     })
 })
