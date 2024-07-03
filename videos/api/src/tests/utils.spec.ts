320e507652b89b035414a3de5f2749a86aebdee7
Scalco7 <fscalco7@gmail.com>
2024-07-03 19:55:15 -0300

    tests refactor - add --verbose

--- videos/api/src/tests/utils.spec.ts
+++ videos/api/src/tests/utils.spec.ts
@@ -1,7 +1,5 @@
 import { getUserIdByToken } from "../utils/getUserIdByToken";
 
-const oldToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM2ZGEyM2ZlLTliOTktNDg4Yy05MmQyLTg1OWI3OWFiNzUxMyIsIm5hbWUiOiJmZWxpcGUiLCJpYXQiOjE3MTk5NjA5NTUsImV4cCI6MTcxOTk4MjU1NX0.--r4pkfsAXAq-_q4YrGVbGYKAUajJoV7PTzgoc9zRiA'
-
 describe('Utils', () => {
     describe('GetUserById', () => {
         test('Should throw Token invalido for token: ""', () => {
@@ -9,6 +7,7 @@ describe('Utils', () => {
         })
 
         test('Should throw Token expirado for expirated token', () => {
+            const oldToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImM2ZGEyM2ZlLTliOTktNDg4Yy05MmQyLTg1OWI3OWFiNzUxMyIsIm5hbWUiOiJmZWxpcGUiLCJpYXQiOjE3MTk5NjA5NTUsImV4cCI6MTcxOTk4MjU1NX0.--r4pkfsAXAq-_q4YrGVbGYKAUajJoV7PTzgoc9zRiA'
             expect(() => { getUserIdByToken(oldToken) }).toThrow('Token expirado')
         })
     })
