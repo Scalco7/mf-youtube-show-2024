5a9c38e5acf7ad4ccd0393df77071585d251e32a
Scalco7 <fscalco7@gmail.com>
2024-07-04 15:03:28 -0300

    videos frontend navigation test

---
+++ videos/frontend/src/tests/navigation.spec.ts
@@ -0,0 +1,53 @@
+import { getParams, getRoute } from "../scripts/utils/navigation";
+
+const pathname = window.location.pathname
+const search = window.location.search
+
+describe('Nagiation', () => {
+    beforeAll(() => {
+        Object.defineProperty(window, 'location', {
+            value: {
+                pathname: jest.fn(),
+                search: jest.fn(),
+            }
+        });
+    });
+
+    afterAll(() => {
+        window.location.pathname = pathname;
+        window.location.search = search
+    });
+
+    describe('getRoute Function', () => {
+        test('Should get the actual route - 1', () => {
+            const currentRoute = '/videos'
+            window.location.pathname = currentRoute
+
+            expect(getRoute()).toBe(currentRoute)
+        })
+
+        test('Should get the actual route - 2', () => {
+            const currentRoute = '/favoritos'
+            window.location.pathname = currentRoute
+
+            expect(getRoute()).toBe(currentRoute)
+        })
+    })
+
+    describe('getParams Function', () => {
+        test('Should get the token parameter', () => {
+            const mockedToken = 'mockedTokenmockedTokenmockedToken'
+            const searchParams = `?token=${mockedToken}`
+            window.location.search = searchParams
+
+            expect(getParams().get('token')).toBe(mockedToken)
+        })
+
+        test('Should be null', () => {
+            const searchParams = ''
+            window.location.search = searchParams
+
+            expect(getParams().get('token')).toBeNull()
+        })
+    })
+})
